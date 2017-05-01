8a92119f59bef82355e0ef9300b2f39b0c928434
Thomas Tuegel <ttuegel@mailbox.org>
2017-05-01 12:09:00 -0500

    Actions.Helpers: split some common helpers off Actions.Git

--- Actions/Git.hs
+++ Actions/Git.hs
@@ -2,22 +2,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Actions.Git (sync1, clone1) where
+module Actions.Git
+  ( sync1, clone1
+  , gitClone, gitPull, gitPush
+  ) where
 
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Foldable (traverse_)
 import Filesystem.Path.CurrentOS hiding (empty, null)
 import Prelude hiding (FilePath)
 import Turtle hiding (Text, skip)
 
+import Actions.Helpers
 import Actions.Types
-import Posix
-
-proc_ :: MonadIO io => Text -> [Text] -> Shell Line -> io ExitCode
-proc_ cmd args inp = do
-  echo (unsafeTextToLine ("; " <> cmd <> " " <> T.intercalate " " args))
-  proc cmd args inp
 
 gitStatus :: MonadIO io => io ExitCode
 gitStatus = do
@@ -32,32 +29,6 @@ gitPull =
 
 gitPush :: MonadIO io => io ExitCode
 gitPush = proc_ "git" ["push", "--porcelain"] empty
-
-cleanup :: MonadIO io => Text -> io ExitCode
-cleanup reason = do
-  traverse_ echo (textToLines (format ("# "%s) reason))
-  userShell <- getUserShell
-  forkAndWait userShell []
-
-skipIfMissing :: MonadIO io => FilePath -> io () -> io ()
-skipIfMissing path go = do
-  exists <- testdir path
-  if exists then go else skip path "does not exist"
-
-skipIfExists :: MonadIO io => FilePath -> io () -> io ()
-skipIfExists path go = do
-  exists <- testdir path
-  if exists then skip path "path exists" else go
-
-announce :: MonadIO io => FilePath -> Maybe Text -> io ()
-announce path message = do
-  let m pref = makeFormat (maybe "" ((<>) pref))
-  printf ("# "%fp%(m ": ")%"\n") path message
-
-skip :: MonadIO io => FilePath -> Text -> io ()
-skip path reason =
-  let message = format ("skipped ("%s%")") reason
-  in announce path (Just message)
 
 status, pull, push :: MonadIO io => io ExitCode
 status = gitStatus .||. cleanup "Please commit your changes"
@@ -88,5 +59,7 @@ gitClone path url =
 clone1 :: FilePath -> FilePath -> Targets -> IO ()
 clone1 path url targets
   | elem path targets =
+    runManaged $ do
+      home >>= pushd
       skipIfExists path $ void $ gitClone path url
   | otherwise = pure ()
---
+++ Actions/Helpers.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Actions.Helpers where
+
+import qualified Data.Text as T
+import Data.Foldable (traverse_)
+import Prelude hiding (FilePath)
+import Turtle hiding (skip)
+
+import Posix
+
+proc_ :: MonadIO io => Text -> [Text] -> Shell Line -> io ExitCode
+proc_ cmd args inp = do
+  printf ("; "%s%" "%s%"\n") cmd (T.intercalate " " args)
+  proc cmd args inp
+
+cleanup :: MonadIO io => Text -> io ExitCode
+cleanup reason = do
+  traverse_ echo (textToLines (format ("# "%s) reason))
+  userShell <- getUserShell
+  forkAndWait userShell []
+
+skipIfMissing :: MonadIO io => FilePath -> io () -> io ()
+skipIfMissing path go = do
+  exists <- testdir path
+  if exists then go else skip path "does not exist"
+
+skipIfExists :: MonadIO io => FilePath -> io () -> io ()
+skipIfExists path go = do
+  exists <- testdir path
+  if exists then skip path "path exists" else go
+
+announce :: MonadIO io => FilePath -> Maybe Text -> io ()
+announce path message = do
+  let m pref = makeFormat (maybe "" ((<>) pref))
+  printf ("# "%fp%(m ": ")%"\n") path message
+
+skip :: MonadIO io => FilePath -> Text -> io ()
+skip path reason =
+  let message = format ("skipped ("%s%")") reason
+  in announce path (Just message)
