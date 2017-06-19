8858233810e9d1829f61b91e7a995d8188ebf2ae
Thomas Tuegel <ttuegel@mailbox.org>
2017-06-19 10:22:08 -0500

    Include nixpkgs submodule

--- default.nix
+++ default.nix
@@ -1,17 +1,41 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import ./nixpkgs {}, compiler ? "default" }:
 
 let
 
   inherit (nixpkgs) pkgs;
-  inherit (pkgs.haskell.lib) dontCheck;
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
+  filterSource =
+    let
+      inherit (pkgs.haskell.lib) overrideCabal;
+      overrideSrc = drv: f:
+        overrideCabal drv (args: args // { src = f args.src; });
+    in
+      drv: pred: overrideSrc drv (src: builtins.filterSource pred src);
 
-  drv = haskellPackages.callPackage ./repos.nix {
-    yaml = dontCheck haskellPackages.yaml_0_8_23;
-  };
+  omitDirs =
+    let
+      blacklistDirs = [ ".git" "dist" "nixpkgs" ];
+      whitelistExts = [ ".cabal" ".hs" ];
+      whitelistNames = [ "LICENSE" ];
+
+      predicate = path: type:
+        let inherit (nixpkgs.lib) any elem hasSuffix; in
+        let baseName = baseNameOf path; in
+        if type == "directory"
+          then !(elem baseName blacklistDirs)
+          else any (suf: hasSuffix suf baseName) whitelistExts
+            || any (name: baseName == name) whitelistNames;
+    in
+      drv: filterSource drv predicate;
+
+  drv = omitDirs (haskellPackages.callPackage ./repos.nix {
+    yaml =
+      let inherit (pkgs.haskell.lib) dontCheck; in
+      dontCheck haskellPackages.yaml_0_8_23_1;
+  });
 
 in
 
