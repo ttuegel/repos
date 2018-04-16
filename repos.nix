13b4490882a6cece2ac9b3e3b7bb9734c8cbbfc1
Thomas Tuegel <ttuegel@mailbox.org>
2018-04-16 08:38:57 -0500

    Use dhall >= 1.12

--- repos.nix
+++ repos.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, bytestring, containers, dhall
+{ mkDerivation, base, bytestring, containers, dhall, formatting
 , insert-ordered-containers, optparse-applicative, stdenv
 , system-filepath, text, text-format, trifecta, turtle, unix
 , vector
@@ -10,9 +10,9 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base bytestring containers dhall insert-ordered-containers
-    optparse-applicative system-filepath text text-format trifecta
-    turtle unix vector
+    base bytestring containers dhall formatting
+    insert-ordered-containers optparse-applicative system-filepath text
+    text-format trifecta turtle unix vector
   ];
   license = stdenv.lib.licenses.unfree;
   hydraPlatforms = stdenv.lib.platforms.none;
